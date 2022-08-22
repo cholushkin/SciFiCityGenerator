0ef3dd96ab5af9ad1f8569f773d1fd554be66263
max <cholushkin@gmail.com>
2022-08-23 01:14:47 +0300

    * Importing 2 chunks (WIP)

---
+++ ScifiCityGeneratorUnity/Assets/SciFiCity/TowerGenerator/Editor/SciFiCityImportSettings.cs
@@ -0,0 +1,14 @@
+using TowerGenerator;
+using UnityEditor;
+
+namespace Game
+{
+    [InitializeOnLoad]
+    public static class SciFiCityImportSettings
+    {
+        static SciFiCityImportSettings()
+        {
+            ChunkImportSettingsManager.RegisterChunkImportSettings("Platform", new ChunkImportSettings { ChunksOutputPath = "Assets/SciFiCity/Chunks/!Prefabs", AddColliders = false, IsPack = false, EnableChunkGeneration = false});
+        }
+    }
+}
