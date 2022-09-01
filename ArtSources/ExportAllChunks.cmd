a2d1bbf9914c3db921d4bc73b7b38ef47c9aa776
max <cholushkin@gmail.com>
2022-09-01 22:08:45 +0300

    + Showroom added.

--- ArtSources/ExportAllChunks.cmd
+++ ArtSources/ExportAllChunks.cmd
@@ -1,4 +1,4 @@
 cd ExportScripts
 call config.cmd
-call export_fbx_object.cmd ../Chunks.blend ChunkCity.000 SciFiCity/!Fbx/ChunkCity.000.fbx
-call export_fbx_object.cmd ../Chunks.blend ChunkCity.001 SciFiCity/!Fbx/ChunkCity.001.fbx
+call export_fbx_object.cmd ../Chunks.blend ChunkCity.000 SciFiCity/TowerGenerator/!Fbx/ChunkCity.000.fbx
+call export_fbx_object.cmd ../Chunks.blend ChunkCity.001 SciFiCity/TowerGenerator/!Fbx/ChunkCity.001.fbx
