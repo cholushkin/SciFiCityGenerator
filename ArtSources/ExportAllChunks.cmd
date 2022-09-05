9d045a596398165e4b5e5c662ac5edf2c941f883
max <cholushkin@gmail.com>
2022-09-05 21:13:39 +0300

    + 1 chunk added. #art

--- ArtSources/ExportAllChunks.cmd
+++ ArtSources/ExportAllChunks.cmd
@@ -2,3 +2,4 @@ cd ExportScripts
 call config.cmd
 call export_fbx_object.cmd ../Chunks.blend ChunkCity.000 SciFiCity/TowerGenerator/!Fbx/ChunkCity.000.fbx
 call export_fbx_object.cmd ../Chunks.blend ChunkCity.001 SciFiCity/TowerGenerator/!Fbx/ChunkCity.001.fbx
+call export_fbx_object.cmd ../Chunks.blend ChunkCity.002 SciFiCity/TowerGenerator/!Fbx/ChunkCity.002.fbx
