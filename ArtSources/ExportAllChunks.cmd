641d2cdb870f4a54d016c46c9e554acaa18d7363
max <cholushkin@gmail.com>
2022-10-18 23:39:11 +0300

    * Chunks #art

--- ArtSources/ExportAllChunks.cmd
+++ ArtSources/ExportAllChunks.cmd
@@ -3,4 +3,4 @@ call config.cmd
 call export_fbx_object.cmd ../Chunks.blend ChunkCity.000 SciFiCity/TowerGenerator/!Fbx/ChunkCity.000.fbx
 call export_fbx_object.cmd ../Chunks.blend ChunkCity.001 SciFiCity/TowerGenerator/!Fbx/ChunkCity.001.fbx
 call export_fbx_object.cmd ../Chunks.blend ChunkCity.002 SciFiCity/TowerGenerator/!Fbx/ChunkCity.002.fbx
-call export_fbx_object.cmd ../Chunks.blend ChunkCity.003 SciFiCity/TowerGenerator/!Fbx/ChunkCity.003.fbx
+rem call export_fbx_object.cmd ../Chunks.blend ChunkCity.003 SciFiCity/TowerGenerator/!Fbx/ChunkCity.003.fbx
